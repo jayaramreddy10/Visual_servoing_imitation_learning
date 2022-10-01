14ee75f0352d41d2895110c64c35c6e3d6f3fc1b
jairam10 <ramreddyai1010@gmail.com>
2022-10-02 00:58:29 +0530

    random change

--- run_imitation_learning.py
+++ run_imitation_learning.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if use_gpu else "cpu")
 
      # Model
-    model = Wav2Lip().to(device)
+    model = IM_VS().to(device)
 
     print('total trainable params {}'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
     optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad],
